5a6f01e53e57fe5a8129f36b8990bd7bcc7c9039
Warnsley <Marcus.Warnsley@navient.com>
2016-04-05 22:04:20 -0400

    adding some of the jquery for the slider initially

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function slidingFunctionality(){
+	//set options
+	var	speed = 500, //fading speed
+		autoswitch = true, //auto slider option
+		autoswitch_speed = 4000; //auto slider speed
+
+	//add initial active class
+	$(".slide").first().addClass("active");
+
+	//hide all slides
+	$(".slide").hide();
+
+	//show the first slide
+	
+}
+
+slidingFunctionality();
